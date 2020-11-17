08e97cfd2bd5b7cc1703caa272df082cbd522355
isaac_lee <yy0221ss@gmail.com>
2020-11-17 13:43:06 +0900

    commit via mac

---
+++ js/login.js
@@ -0,0 +1,16 @@
+// 위에서부터 환자 로그인, 간호사 로그인, 회원가입 버튼
+patientLoginBtn = document.getElementById();
+nurseLoginBtn = document.getElementById();
+siginBtn = document.getElementById();
+
+// 회원가입 페이지로 이동하는 함수
+function gotoSignIn() {}
+
+// 환자의 로그인이 성공하면 환자 홈으로 이동하는 함수
+function patientLogin() {}
+
+// 간호사의 로그인이 성공하면 간호사 홈으로 이동하는 함수
+function nurseLogin() {}
+
+// 로그인 검증하는 함수
+function validate() {}
